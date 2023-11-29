500c4a15bb34ef9971b54ea438df797fcc68ce37
vkvech <vasekkvech2003@gmail.com>
2023-11-29 21:23:48 +0100

    doucko 2 ukol doma

---
+++ script.js
@@ -0,0 +1,16 @@
+const seznam= [
+    {jmeno:"Petr", prijmeni:"Breit", rocnik:"1"},
+    {jmeno:"Vašek", prijmeni:"Kvěch", rocnik:"2"}
+];
+
+function vypisDoTabulky()
+{
+    let table = document.getElementById("studentTable");
+
+
+}
+
+function pridat()
+{
+    vypisDoTabulky()
+}
